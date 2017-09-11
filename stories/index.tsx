c06bfd204be06f7e809ca091044e416492445921
Nathanial Hartman <nathanial.hartman@gmail.com>
2017-09-11 02:49:57 -0700

    added todo column view

--- stories/index.tsx
+++ stories/index.tsx
@@ -3,17 +3,64 @@ import * as _ from 'lodash';
 
 import "normalize.css/normalize.css";
 import "@blueprintjs/core/dist/blueprint.css";
+import '../src/index.css';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import TodoView from '../src/components/todo/TodoView';
+import TodoColumnView from '../src/components/todo/TodoColumnView';
 import * as Freezer from 'freezer-js';
 
 import { Button, Welcome } from '@storybook/react/demo';
 
+type Props = {
+  getFreezerState(): any;
+}
+
+
+const freezer = new Freezer({
+  todo: {},
+  column: {
+    name: 'Example',
+    todos: [
+      {name: 'Foo', id: 'foo'},
+      {name: 'Bar', id: 'bar'}
+    ]
+  }
+});
+
+class FreezerComponent extends React.Component<Props,{}> {
+
+  constructor(props){
+    super(props);
+    freezer.on('update', () => {
+      this.forceUpdate();
+    });
+  }
+
+  render(){
+    return (
+      <div className="pt-dark" style={{position: 'absolute', top: 0, bottom: 0}}> 
+        {React.Children.map(this.props.children, element => React.cloneElement(element as any, this.props.getFreezerState()))}
+      </div>
+    );
+  }
+
+}
 storiesOf('TodoView', module)
 .add('default', () => {
-  const todo = new Freezer({}).get();
-  return <TodoView todo={todo} confirmDeletion={false} onDelete={_.noop} />
+  return (
+    <FreezerComponent getFreezerState={() => ({todo: freezer.get().todo})}>
+      <TodoView todo={freezer.get().todo} confirmDeletion={false} onDelete={action("Delete Todo")} />
+    </FreezerComponent>
+  );
 });
+storiesOf('TodoColumnView', module)
+.add('default', () => {
+  return (
+    <FreezerComponent getFreezerState={() => ({column: freezer.get().column})}>
+      <TodoColumnView column={freezer.get().column} onDeleteColumn={action("Delete Column")} />
+    </FreezerComponent>
+  );
+})
