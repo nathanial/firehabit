d4002df0182a0b67dc63d36f08a1233f28fc3888
Nathanial Hartman <nathanial.hartman@gmail.com>
2018-11-20 03:39:06 -0800

    stubbing out sidebar

--- src/components/todo/TodoSidebar.tsx
+++ src/components/todo/TodoSidebar.tsx
@@ -9,13 +9,17 @@ export default class TodoSidebar extends React.PureComponent<Props> {
     render(){
         return (
           <div className="todo-sidebar">
-              <div className={"sidebar-button active"}>
+              <div className={"sidebar-button active"} onClick={() => this.onSidebarButtonClicked("column-view")}>
                   <Icon iconName={"comparison"} />
               </div>
-              <div className={"sidebar-button"}>
+              <div className={"sidebar-button"} onClick={() => this.onSidebarButtonClicked("list-view")}>
                 <Icon iconName={"numbered-list"}/>
               </div>
           </div>
         )
+    }
+
+    private onSidebarButtonClicked = (button: string) => {
+        console.log("Sidebar Button", button);
     }
 }
